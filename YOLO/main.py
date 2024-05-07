1d094a0924d2bdcd86330a4ea609585f330100ee
Belalaskaik <110508413+Belalaskaik@users.noreply.github.com>
2024-05-06 19:48:42 -0700

    Delete YOLO directory

--- YOLO/main.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8.yaml") # build a new model from scratch
-
-# Use the model
-results = model.train(data="config.yaml", epochs=1) # train the model
-
